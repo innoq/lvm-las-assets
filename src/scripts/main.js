1f2f6971ac3de404fff205bdec98f1743ff71280
Lucas Dohmen <lucas@dohmen.io>
2016-03-11 12:44:29 +0100

    Adjust code style

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,83 +1,79 @@
-/* jshint node: true, browser: true */
-"use strict";
+const $ = require('jquery')
+// shim required for non-CommonJS libraries
+global.jQuery = global.$ = $
+// most widgets initialized via markup
+require('bootstrap')
+const shortcutsInfo = require('./shortcuts')
+const OneBox = require('./onebox')
+const StickyNav = require('stickynav')
+const collectionPreview = require('./collection_preview')
+// XXX: bad name
+const gridSelector = require('./grid_selector')
+const ediTable = require('./editable')
+const Accordion = require('./accordion')
+const listing = require('./listing')
 
-var $ = require("jquery");
-global.jQuery = global.$ = $; // shim required for non-CommonJS libraries
-require("bootstrap"); // most widgets initialized via markup
-var shortcutsInfo = require("./shortcuts");
-var OneBox = require("./onebox");
-var StickyNav = require("stickynav");
-var collectionPreview = require("./collection_preview");
-var gridSelector = require("./grid_selector"); // XXX: bad name
-var ediTable = require("./editable");
-var Accordion = require("./accordion");
-var listing = require("./listing");
+shortcutsInfo()
 
-shortcutsInfo();
+$('.onebox').each((i, node) => {
+  new OneBox(node)
+})
 
-$(".onebox").each(function(i, node) {
-    new OneBox(node);
-});
+$('a.collection-preview').each((i, node) => {
+  new collectionPreview(node)
+})
 
-$("a.collection-preview").each(function(i, node) {
-    new collectionPreview(node);
-});
+$('.expandable-grid').each((i, node) => { // XXX: bad class name
+  gridSelector(node)
+})
 
-$(".expandable-grid").each(function(i, node) { // XXX: bad class name
-    gridSelector(node);
-});
-
-$("table.extensible").each(function(i, node) {
-    new ediTable(node);
-});
+$('table.extensible').each((i, node) => {
+  new ediTable(node)
+})
 
 $(() => {
-  $("[data-accordion='active']").each((i, node) => { new Accordion(node) })
+  $('[data-accordion=active]').each((i, node) => { new Accordion(node) })
 
-  new StickyNav($("[data-position='sticky']"), {
-    fixedClass: "is-fixed"
+  new StickyNav($('[data-position=sticky]'), {
+    fixedClass: 'is-fixed'
   })
 })
 
-$("table, ul, ol").filter(".sortable, .filterable").each(function(i, node) {
-    var wrapper = listing(node);
-    // app-specific adjustments
-    // XXX: inelegant, but should be rendered (and functional) server-side anyway
-    var filter = wrapper.find("input.search").attr("placeholder", "Filtern");
-    var header = wrapper.prev(".listing-header");
-    if(header.length === 0) { // responsive table
-        header = wrapper.closest(".table-responsive").prev(".listing-header");
-    }
-    if(filter.length && header.length) {
-        filter.appendTo(header);
-    }
-});
+$('table, ul, ol').filter('.sortable, .filterable').each((i, node) => {
+  const wrapper = listing(node)
+  // app-specific adjustments
+  // XXX: inelegant, but should be rendered (and functional) server-side anyway
+  const filter = wrapper.find('input.search').attr('placeholder', 'Filtern')
+  let header = wrapper.prev('.listing-header')
+  if (header.length === 0) { // responsive table
+    header = wrapper.closest('.table-responsive').prev('.listing-header')
+  }
+  if (filter.length && header.length) {
+    filter.appendTo(header)
+  }
+})
 
 // initialize Bootstrap widgets
 
-$("[data-toggle=tooltip]").tooltip(); // XXX: inefficient selector
+$('[data-toggle=tooltip]').tooltip()
 
-$("[data-toggle=popover]").popover({ // XXX: inefficient selector
-    trigger: "hover focus",
-    container: "body"
-});
+$('[data-toggle=popover]').popover({
+  trigger: 'hover focus',
+  container: 'body'
+})
 
 // ensure drop-down menus do not extend beyond the viewport
-$(".dropdown").on("shown.bs.dropdown", function(ev, ctx) {
-    var menu = $(".dropdown-menu", this);
-    ensureVisible(menu);
-});
+$('.dropdown').on('shown.bs.dropdown', (ev, ctx) => {
+  const menu = $('.dropdown-menu', this)
+  const max = $(window).width()
+  const right = menu.offset().left + menu.outerWidth()
+  if (right > max) {
+    menu.css('transform', 'translateX(' + (max - right) + 'px)')
+  }
+})
 
 // Clone some markup in order to avoid styling for breakpoints in JS
 // Also see src/styles/customer-card.less
-$(".customer-card-portfolio").clone().prependTo(".customer-card-details");
-$(".customer-card-flags").clone().prependTo(".customer-card-details");
-$(".customer-card-drawer").clone().appendTo(".customer-card-details");
-
-function ensureVisible(el) {
-    var max = $(window).width();
-    var right = el.offset().left + el.outerWidth();
-    if(right > max) {
-        el.css("transform", "translateX(" + (max - right) + "px)");
-    }
-}
+$('.customer-card-portfolio').clone().prependTo('.customer-card-details')
+$('.customer-card-flags').clone().prependTo('.customer-card-details')
+$('.customer-card-drawer').clone().appendTo('.customer-card-details')
