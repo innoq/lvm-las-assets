5d22479d5141f5949d0015f2b5ec3aae8331b1f9
Lucas Dohmen <lucas@dohmen.io>
2016-03-11 12:46:47 +0100

    Execute code on DOM ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,67 +13,67 @@ const ediTable = require('./editable')
 const Accordion = require('./accordion')
 const listing = require('./listing')
 
-shortcutsInfo()
-
-$('.onebox').each((i, node) => {
-  new OneBox(node)
-})
-
-$('a.collection-preview').each((i, node) => {
-  new collectionPreview(node)
-})
-
-$('.expandable-grid').each((i, node) => { // XXX: bad class name
-  gridSelector(node)
-})
-
-$('table.extensible').each((i, node) => {
-  new ediTable(node)
-})
-
 $(() => {
+  shortcutsInfo()
+
+  $('.onebox').each((i, node) => {
+    new OneBox(node)
+  })
+
+  $('a.collection-preview').each((i, node) => {
+    new collectionPreview(node)
+  })
+
+  $('.expandable-grid').each((i, node) => { // XXX: bad class name
+    gridSelector(node)
+  })
+
+  $('table.extensible').each((i, node) => {
+    new ediTable(node)
+  })
+
   $('[data-accordion=active]').each((i, node) => { new Accordion(node) })
 
   new StickyNav($('[data-position=sticky]'), {
     fixedClass: 'is-fixed'
   })
+
+  $('table, ul, ol').filter('.sortable, .filterable').each((i, node) => {
+    const wrapper = listing(node)
+    // app-specific adjustments
+    // XXX: inelegant, but should be rendered (and functional) server-side anyway
+    const filter = wrapper.find('input.search').attr('placeholder', 'Filtern')
+    let header = wrapper.prev('.listing-header')
+    if (header.length === 0) { // responsive table
+      header = wrapper.closest('.table-responsive').prev('.listing-header')
+    }
+    if (filter.length && header.length) {
+      filter.appendTo(header)
+    }
+  })
+
+  // initialize Bootstrap widgets
+
+  $('[data-toggle=tooltip]').tooltip()
+
+  $('[data-toggle=popover]').popover({
+    trigger: 'hover focus',
+    container: 'body'
+  })
+
+  // ensure drop-down menus do not extend beyond the viewport
+  $('.dropdown').on('shown.bs.dropdown', (ev, ctx) => {
+    const menu = $('.dropdown-menu', this)
+    const max = $(window).width()
+    const right = menu.offset().left + menu.outerWidth()
+    if (right > max) {
+      menu.css('transform', 'translateX(' + (max - right) + 'px)')
+    }
+  })
+
+  // Clone some markup in order to avoid styling for breakpoints in JS
+  // Also see src/styles/customer-card.less
+  $('.customer-card-portfolio').clone().prependTo('.customer-card-details')
+  $('.customer-card-flags').clone().prependTo('.customer-card-details')
+  $('.customer-card-drawer').clone().appendTo('.customer-card-details')
 })
-
-$('table, ul, ol').filter('.sortable, .filterable').each((i, node) => {
-  const wrapper = listing(node)
-  // app-specific adjustments
-  // XXX: inelegant, but should be rendered (and functional) server-side anyway
-  const filter = wrapper.find('input.search').attr('placeholder', 'Filtern')
-  let header = wrapper.prev('.listing-header')
-  if (header.length === 0) { // responsive table
-    header = wrapper.closest('.table-responsive').prev('.listing-header')
-  }
-  if (filter.length && header.length) {
-    filter.appendTo(header)
-  }
-})
-
-// initialize Bootstrap widgets
-
-$('[data-toggle=tooltip]').tooltip()
-
-$('[data-toggle=popover]').popover({
-  trigger: 'hover focus',
-  container: 'body'
-})
-
-// ensure drop-down menus do not extend beyond the viewport
-$('.dropdown').on('shown.bs.dropdown', (ev, ctx) => {
-  const menu = $('.dropdown-menu', this)
-  const max = $(window).width()
-  const right = menu.offset().left + menu.outerWidth()
-  if (right > max) {
-    menu.css('transform', 'translateX(' + (max - right) + 'px)')
-  }
-})
-
-// Clone some markup in order to avoid styling for breakpoints in JS
-// Also see src/styles/customer-card.less
-$('.customer-card-portfolio').clone().prependTo('.customer-card-details')
-$('.customer-card-flags').clone().prependTo('.customer-card-details')
-$('.customer-card-drawer').clone().appendTo('.customer-card-details')
