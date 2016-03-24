7cac9e552d5f0092fc2b5f9ad2827340c6b63058
Lucas Dohmen <lucas@dohmen.io>
2016-03-24 18:13:21 +0100

    Make the window optional

--- components/preview/index.js
+++ components/preview/index.js
@@ -15,23 +15,25 @@ module.exports = (el) => {
     const selector = el.data('preview-selector')
     const content = $(selector, $.parseHTML(responseHtml))
 
-    if (el.data('preview-count-selector')) {
-      const count = content.find(el.data('preview-count-selector')).length
+    if (el.data('preview-count')) {
+      const count = content.find(el.data('preview-count')).length
       el.attr('data-bubble', count)
     }
 
-    el.data({
-      content: content,
-      html: true,
-      placement: 'bottom',
-      title: renderPreviewTitle(el.data('preview-title'), el.attr('href')),
-      toggle: 'popover'
-    }).on('click', (e) => {
-      e.preventDefault()
-    }).popover({
-      container: 'body',
-      trigger: 'click'
-    })
+    if (el.data('preview-window') === '') {
+      el.data({
+        content: content,
+        html: true,
+        placement: 'bottom',
+        title: renderPreviewTitle(el.data('preview-title'), el.attr('href')),
+        toggle: 'popover'
+      }).on('click', (e) => {
+        e.preventDefault()
+      }).popover({
+        container: 'body',
+        trigger: 'click'
+      })
+    }
   }).fail(() => {
     el.attr('data-bubble', '!')
     el.data({
